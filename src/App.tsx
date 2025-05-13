46eb09c7d0e5bb85160a831558d376ba387cbdfe
victor071246 <daderiovictorfelipe@gmail.com>
2025-05-13 20:38:02 -0300

    Site portfólio finalizado e cores consertadas/com link github

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const myProjects = `<code>&nbsp;&nbsp;&nbsp;&nbsp;<strong>Site Portfólio Pitera
 Site portfólio para a empresa PiteraBOX, com conteúdo renderizado dinamicamente com no MongoDB (Atlas), Crud completo e API feitas com base em JavaScript e no FrameWork Express. FrontEnd construído com HTML e CSS padrão com destaque para o design responsivo de celular.
 
 <u>[[Projeto no GitHub]](https://github.com/victor071246/ProjetoIntegradorTestes/tree/main)</u>
-<u>[[PiteraBOX no Render]](https://projetointegradortestes.onrender.com/index.html) </u>
+<u>[[PiteraBOX no Render]](https://projetointegradortestes.onrender.com/index.html)</u>
 
 
 <hr>
@@ -21,7 +21,7 @@ Site portfólio para a empresa PiteraBOX, com conteúdo renderizado dinamicament
 Aplicação desenvolvida com ReactJS, com foco em componentes reutilizáveis e estilização modular. O projeto visa apresentar fluência na biblioteca React e conceitos modernos de desenvolvimento web.
 
 <u>[[GitHub do Desafio React]](https://github.com/victor071246/desafio_react/tree/master)</u> 
-<u>[[Desafio React no Vercel]](https://desafio-react-ruddy.vercel.app/) </u>
+<u>[[Desafio React no Vercel]](https://desafio-react-ruddy.vercel.app/)</u>
 
 
 <hr>
@@ -41,10 +41,9 @@ Projeto desenvolvido como parte de um desafio de estágio, utilizando HTML5 e CS
 Portfólio desenvolvido com TypeScript, React e TailWindCSS. Foi criado um hook personalizado que faz tanto a digitação dos itens, sua velocidade de digitação, animação de cursor e três pontos no final do texto. Também foi construído um hook que alterna o tema entre claro e escuro
 
 <u>[[GitHub deste projeto]](https://github.com/victor071246/site-terminal-typescript-tailwind)</u>
-<u>[[Projeto no Vercel]]()</u>
-<code>`;
+<u>[[Projeto no Vercel]](https://site-terminal-typescript-tail-git-bc4efc-victor071246s-projects.vercel.app/)</u><code>`;
 
-const about = `<code>&nbsp&nbsp&nbsp&nbspTenho 22 anos, curso Engenharia da Computação e tenho Técnico em Informática. Foco meus projetos principalmente em JavaScript e agora venho estudando TypeScript.</code>`;
+const about = `<code>&nbsp&nbsp&nbsp&nbspTenho 22 anos, curso Engenharia da Computação e tenho Técnico em Informática. Foco meus projetos principalmente em JavaScript e agora venho estudando TypeScript</code>`;
 
 const profissionalExperience = `<code>&nbsp&nbsp&nbsp&nbspAuxiliar Administrativo Jovem Aprendiz em Departamento Autônomo de Água e Esgoto de Araraquara (07/2022 - 12/2023)</code>`;
 
